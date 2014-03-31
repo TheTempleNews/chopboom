8242a5c335d4e978312262318f7960fb1c4aceb2
Chris Montgomery <chris@montchr.io>
2014-03-31 16:36:54 -0400

    disable sidebar on single and pages

--- lib/config.php
+++ lib/config.php
@@ -55,7 +55,9 @@ function roots_display_sidebar() {
      */
     array(
       'is_404',
-      'is_front_page'
+      'is_front_page',
+      'is_single',
+      'is_page'
     ),
     /**
      * Page template checks (via is_page_template())
