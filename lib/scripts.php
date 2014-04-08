da123cb03221d45a2a0f052ed005be446d53e7fb
robdirienzo <robd@temple.edu>
2014-04-07 21:10:33 -0400

    Started Timeline

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e07804fd4e893150fe564795b37c1bc5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5d1e63b0f3c09fe6e4e5f02c18f7dcac');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
