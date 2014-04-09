cf89c0e401e6ca1bff0da635c68dbb1e48486a4b
Chris Montgomery <chris@montchr.io>
2014-04-09 19:38:51 -0400

    make sure front page styles only affect front page
    - wrap all scss in `.home` body class
    - fix indentation (from a copy/paste operation)
    - ask pertinent questions like "what is numVideos?"
    - that stupid version hash in `lib/scripts.php`

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '69f9e78b9ab492b5e664fca16e33d25a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6121831aa45bc8cfad7d10e1d2e8c44');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
