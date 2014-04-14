c863419fccb43c028f4526fde5c03e95321452cd
Chris Montgomery <chris@montchr.io>
2014-04-13 22:05:35 -0400

    2up center layout, rename sup to cutnado

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2515a1e81992a05e5dc9d72c10fed47f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '659dd1fc32adafd2777acefe57f4158a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
