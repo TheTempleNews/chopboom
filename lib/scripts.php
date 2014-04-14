bd9cc5b92106758574003fdd64b3ccd9dd2468c7
Chris Montgomery <chris@montchr.io>
2014-04-13 20:11:33 -0400

    clearing, clearfix, less specific resets

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '61bcda7497abfb52e44caf2cf14674c2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2515a1e81992a05e5dc9d72c10fed47f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
