18004ead6a7b88156dde5f7184c90335660272fa
Chris Montgomery <chris@montchr.io>
2014-05-08 18:51:16 -0400

    keep headings aligned properly

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0298f5c5fea9542e45cc23ccffe12038');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4a5ec2eb410c9bc98c8d680457035db7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
