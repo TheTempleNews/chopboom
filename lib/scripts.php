c5a69bf94e43321b16351fcafbfa239e6510e872
Chris Montgomery <chris@montchr.io>
2014-05-09 23:10:23 -0400

    no left and right padding on component content

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1430477b00229a6f7402eab5e360b091');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'defa7b2783f2e8482cc55e7c78cc976f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
