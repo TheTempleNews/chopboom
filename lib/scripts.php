1a62a1603213ed74587fdd59572ec1f9366f3736
Chris Montgomery <chris@montchr.io>
2014-05-09 22:58:47 -0400

    refinements for chapter titles

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cbe4160abe75abe10739b9059d7905c5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1430477b00229a6f7402eab5e360b091');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
