a482ecee2f42e7642414a63c4a1d5b6947a48e56
Chris Montgomery <chris@montchr.io>
2014-05-10 17:45:44 -0400

    repositioning of front page text

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '425d6631112f9a4d951e2e785795f979');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a0e49fb0fb9bdb147faa6444f3e00041');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
