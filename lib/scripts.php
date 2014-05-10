053ce624246173924e8b48e6fc8ec5af2cdd510a
Chris Montgomery <chris@montchr.io>
2014-05-09 20:25:47 -0400

    drop caps and leads

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '23e02d1a70d9dcf28238f040b8f29c5f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4a3da5c9dabbcfbb79684b97dedf0d31');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
