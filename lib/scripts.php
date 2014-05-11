3bb4550deeca3d08c7a9f898c43d79f79a6b3c79
Chris Montgomery <chris@montchr.io>
2014-05-11 11:44:54 -0400

    and there's the footer, fixes #24 and #35

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c46ae74169aa9392f29d2cbeed0db60d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd93543aab401dda42ea00bc012d1893c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,11 @@
 <footer class="content-info" role="contentinfo">
-  <p>
-    &copy; <?php echo date('Y'); ?> The Temple News<br>
-    <span class="first-published">First published on May 11, 2014</span><br>
-    <span class="last-updated">Last updated on <?php the_modified_time('F d, Y'); ?> at <?php the_modified_time('H:i'); ?></span>
-  </p>
+  <div class="inner">
+    <p>
+      &copy; <?php echo date('Y'); ?> The Temple News<br>
+      <span class="first-published">First published on May 11, 2014</span><br>
+      <span class="last-updated">Last updated on <?php the_modified_time('F d, Y'); ?> at <?php the_modified_time('H:i'); ?></span>
+    </p>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
