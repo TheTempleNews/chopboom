04ed21374efd44712fe18c49f82889aa5994fd9b
Chris Montgomery <chris@montchr.io>
2014-05-12 15:26:34 -0400

    now we can call this responsive, or something

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '46619e02b320f3b10c0cbb3f21f1a42a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6494d6ae089356908a744190bea2967');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
