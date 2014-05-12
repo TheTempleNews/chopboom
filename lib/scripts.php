bccad4a1749280f69711161bc18f3663f54ef7a0
Chris Montgomery <chris@montchr.io>
2014-05-12 13:04:40 -0400

    static width, first shot, fixes #25

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b532f6266f894dfc206b5194a3ee2c9a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c861e8aaefd3916981cca0ab5bb25352');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
