8f10ebdb6a1cafb39f7d80c4ead3b2b44c082555
Chris Montgomery <chris@montchr.io>
2014-05-12 14:50:29 -0400

    implement media queries and whatnot

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1b0e21f94af1fd6ef4863672eeaa3dd7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0720b38f1575a2fe07403472cc4c4de9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
