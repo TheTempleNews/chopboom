4e8f163286a3411dc0344e5fef00ed8ab12abec3
Chris Montgomery <chris@montchr.io>
2014-05-12 00:22:04 -0400

    re-enable twbs nav component

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '89fa7fa6ff4466cd0ee4b42eae8de708');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8df1d94d88d7d8e3d4289040636aeeb5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
