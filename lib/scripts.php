109473591d57a0495373e2c89d7fe01e960e9ac9
Chris Montgomery <chris@montchr.io>
2014-05-12 08:54:24 -0400

    move 'fighting back' title slightly up, fixes #38

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '593ef7def256cc39a764a123d50ebba4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b532f6266f894dfc206b5194a3ee2c9a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
